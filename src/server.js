359cf99c4a3064e419da5af901da8a7033d81721
jonry <jonrysimbolon19@gmail.com>
2024-08-20 22:11:35 +0700

    best practice handle error

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
-const NotesValidator = require('./validator/notes')
+const NotesValidator = require('./validator/notes');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const notesService = new NotesService();
@@ -19,7 +20,7 @@ const init = async () => {
     plugin: notes,
     options: {
       service: notesService,
-      validator: NotesValidator
+      validator: NotesValidator,
     },
   });
 
@@ -39,6 +40,23 @@ const init = async () => {
 
     return h.continue;
   });*/
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+
+    const { response } = request;
+    
+    // penangangan client error secara internal
+    if(response instanceof ClientError){
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+    return h.continue;
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
